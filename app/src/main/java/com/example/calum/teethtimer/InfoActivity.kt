b93141e8ab90a56cf291fedd75edd861386887d6
Calum McCall <calumcmccall@gmail.com>
2018-12-04 18:25:32 +0000

    Info Activity Action Bar
    Action bar for info activity added with up navigation

--- app/src/main/java/com/example/calum/teethtimer/InfoActivity.kt
+++ app/src/main/java/com/example/calum/teethtimer/InfoActivity.kt
@@ -2,11 +2,16 @@ package com.example.calum.teethtimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_alarms.*
 
 class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
