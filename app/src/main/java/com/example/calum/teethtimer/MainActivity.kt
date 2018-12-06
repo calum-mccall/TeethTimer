69630583f8a9b2e58da636c16e10f419cfec943b
Calum McCall <calumcmccall@gmail.com>
2018-12-06 19:27:49 +0000

    Cleaned MainActivity.kt
    Removed unused code from MainActivity.kt

--- app/src/main/java/com/example/calum/teethtimer/MainActivity.kt
+++ app/src/main/java/com/example/calum/teethtimer/MainActivity.kt
@@ -19,24 +19,16 @@ class MainActivity : AppCompatActivity() {
 
     var confettiSelector = ConfettiTypes()
 
-    var notifications = Notifications(this)
-
     var timerRunning = false
     var isPaused = false
 
     val TAG = "Main Activity"
-
-    var morningAlarm = ""
-    var eveningAlarm = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(main_toolbar)
-
-        val sharedPreferences = this.getSharedPreferences(R.string.preference_file_key.toString(), Context.MODE_PRIVATE)
-        //showSetAlarms(sharedPreferences)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -71,8 +63,6 @@ class MainActivity : AppCompatActivity() {
 
                 if (isPaused) {
                     waveView.centerTitle = "Paused"
-                    //waveLoadingView.centerTitle = "Pause"
-                    //textView_timer.text = "Paused"
                     resumeFromMillis = millisInFuture
                     cancel()
                 } else {
@@ -87,18 +77,13 @@ class MainActivity : AppCompatActivity() {
 
                     if (remainingMinutes < 1 && remainingSeconds < 10) {
                         waveView.centerTitle = String.format("%01d", remainingSeconds)
-                        //waveView.centerTitleSize += 1
                     }
-                    //textView_timer.text = String.format("%01d:%02d", remainingMinutes, remainingSeconds)
-                    //textView_timer.text = (remainingMinutes).toString() + ":" + (remainingSeconds).toString()
                 }
             }
 
             //Once the timer finishes
             override fun onFinish() {
                 waveView.centerTitle = "Finished"
-                //waveLoadingView.centerTitle = "Finished"
-                //textView_timer.text = "Finished"
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
                 resumeFromMillis = 0
@@ -131,7 +116,6 @@ class MainActivity : AppCompatActivity() {
 
         timerRunning = true
         isPaused = false
-        //button_start_pause.text = "Pause"
 
         button_start.visibility = View.INVISIBLE
         button_alarm_fragment.visibility = View.INVISIBLE
@@ -176,8 +160,6 @@ class MainActivity : AppCompatActivity() {
         waveView.startAnimation()
         timerRunning = false
         isPaused = false
-        //waveLoadingView.centerTitle = ""
-        //textView_timer.text = ""
         resumeFromMillis = 0
         button_start.isEnabled = true
 
@@ -186,11 +168,6 @@ class MainActivity : AppCompatActivity() {
         button_pause_resume.visibility = View.INVISIBLE
         button_reset.visibility = View.INVISIBLE
     }
-
-    /*fun showSetAlarms(sharedPreferences: SharedPreferences) {
-        textView_morning_alarm.text = sharedPreferences.getString(R.string.morning_alarm_time.toString(), morningAlarm)
-        textView_evening_alarm.text = sharedPreferences.getString(R.string.evening_alarm_time.toString(), eveningAlarm)
-    }*/
 
     fun showAlarms(view: View) {
         val intent = Intent(this, AlarmsActivity::class.java)
